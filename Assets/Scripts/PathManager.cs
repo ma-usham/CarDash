5aa9d1b3988ee9a7e9c493b017e1804fc8cce950
Mausham <neupanemausham2060@gmail.com>
2024-12-30 23:26:50 +0545

    batches reduced for smoothgameplay

---
+++ Assets/Scripts/PathManager.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField] private GameObject roadPrefab;
+    [SerializeField] private Transform player;
+    [SerializeField] private int initialRoadCount = 15;
+    [SerializeField] private float roadLength = 3.2f;
+    private Vector3 spawnRoadPos =Vector3.zero;
+
+    private List<GameObject> roadSections = new List<GameObject>();
+    private void Start()
+    {
+        for(int i = 0; i < initialRoadCount; i++)
+        {
+            SpawnRoad();
+        }
+    }
+    private void Update()
+    {
+        if (player.transform.position.z > roadSections[0].transform.position.z + roadLength)
+        {
+            SpawnRoad();
+            DestroyRoad();
+        }
+    }
+
+    private void SpawnRoad()
+    {
+        GameObject road = Instantiate(roadPrefab, spawnRoadPos, Quaternion.identity);
+        road.transform.parent = this.transform;
+       // road.isStatic = true;
+        roadSections.Add(road);
+        spawnRoadPos += Vector3.forward * roadLength;
+    }
+
+    private void DestroyRoad()
+    {
+        GameObject oldRoad = roadSections[0];
+        roadSections.RemoveAt(0);
+        Destroy(oldRoad);
+    }
+
+
+}
